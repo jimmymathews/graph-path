4085e936d841c0e25d9d2b9f21d1ad5a7a3cdaff
jimmymathews <jimmy@jmath.read-books.org>
2020-05-02 01:54:54 -0400

    added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+red="\u001b[0;31m"
+grn="\u001b[0;32m"
+bold_res="\u001b[0m\u001b[1m"
+res="\u001b[0m"
+
+# args: file should_be_present
+remove_if_not_missing() {
+    the_file="$1"
+    if [ $# -gt 1 ]; then
+        should_be_present="$2"
+    else
+        should_be_present="true"
+    fi
+    if [[ -f $the_file ]]; then
+        rm $the_file
+        echo -e "$bold_res$the_file$res$grn removed.$res"
+    else
+        if [[ "$should_be_present" == "true" ]]; then
+            echo -e "$bold_res$the_file$res$red was missing anyway.$res"
+        fi
+    fi
+}
+
+remove_if_not_missing "$HOME/bin/graph-path"
+remove_if_not_missing "$HOME/bin/gp"
+remove_if_not_missing "$HOME/man/man1/graph-path.1.gz"
+remove_if_not_missing "$HOME/man/man1/gp.1.gz"
+remove_if_not_missing "$HOME/man/man1/graph-path.1" false
+remove_if_not_missing "$HOME/man/man1/gp.1" false
