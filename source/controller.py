c526a18e63fbaaf3c1389c18c8c7ba97db186953
jimmymathews <jimmy@jmath.read-books.org>
2020-05-04 01:05:52 -0400

    basic mvc refactoring complete, and zip executable for installation

--- source/controller.py
+++ source/controller.py
@@ -1,12 +1,23 @@
 
-from pygtrie import *  # third-party library
+from pygtrie import *
+from simple_utilities import *
 from terminal_utilities import *
 from model import GPModel
 from view import GPView
 
 class GPController:
-    def __init__(self):
-        self.cstate = ''
+    def __init__(self, graph, vertical_layout, description_capable, case_insensitive):
+        self.tick=0
+        self.vertical_layout = vertical_layout
+        self.description_capable = description_capable
+        self.case_insensitive = case_insensitive
+        case_manager = lambda x : x
+        if(self.case_insensitive):
+            case_manager = lambda x : x.upper()
+        node_names = sorted([case_manager(graph.vs[i]['name']) for i in range(len(graph.vs))])
+        self.model = GPModel(graph, node_names)
+        self.view = GPView(self.model, vertical_layout, description_capable)
+        self.reset_state()
 
     def handle_io(self):
         with CursorOff():
@@ -17,14 +28,15 @@ class GPController:
                 self.view.refresh()
 
     def reset_state(self):
-        self.nodes = ['']
-        self.completion = None
-        self.lines = 1
+        self.model.reset_state()
+        self.view.reset_state()
         self.candidate = 0
         self.pushed = None
         self.neighbor = None
 
     def handle_char_input(self, c):
+        ns = self.model.nodes
+
         if c!='\t':
             self.pushed = None
             self.candidate = 0
@@ -36,78 +48,83 @@ class GPController:
                 self.advance_editor()
             self.tick = self.tick + 1
             if(self.tick == 2):
-                # print('\n'*(self.lines-1))
-                self.display_abridged_cached()
+                self.view.display_abridged_cached()
                 self.reset_state()
             if(self.tick == 3):
-                for i in range(self.lines - 1):
+                for i in range(self.view.lines - 1):
                     print(CLEAR_LINE)
                 exit()
             return
         self.tick = 0
 
-        if c=='\t' and (self.nodes[-1] == '' or self.neighbor is not None):
+        if c=='\t' and (ns[-1] == '' or self.neighbor is not None):
             self.complete_with_neighbors()
             return
-        elif c=='\t' and self.completion is not None:
+        elif c=='\t' and self.model.completion is not None:
             self.auto_complete()
             return
         
-        if c=='' or c==' ' or (c=='\t' and self.completion==''):
+        if c=='' or c==' ' or (c=='\t' and self.model.completion==''):
             self.advance_editor()
         elif ord(c) >= 32 and ord(c) <= 126:
             if self.case_insensitive:
                 c = c.upper()
             self.append(c)
         elif ord(c) == 127:
-            if(self.nodes[-1] == ''):
+            if(ns[-1] == ''):
                 self.regress_editor()
             else:
                 self.lop_off_char()
 
     def complete_with_neighbors(self):
-        if len(self.nodes) <= 1:
+        ns = self.model.nodes
+        if len(ns) <= 1:
             return
-        neighbors = self.graph.neighbors(self.graph.vs.select(name=self.nodes[-2])[0])
+        g = self.model.graph
+        neighbors = g.neighbors(g.vs.select(name=ns[-2])[0])
         if self.neighbor is not None:
             self.neighbor = (self.neighbor + 1) % len(neighbors)
         else:
             self.neighbor = 0
-        self.nodes[-1] = self.graph.vs[neighbors[self.neighbor]]['name']
-        self.completion = ''
-        self.refresh()
+        ns[-1] = g.vs[neighbors[self.neighbor]]['name']
+        self.model.completion = ''
+        self.view.refresh()
 
     def auto_complete(self):
-        if len(self.completion) > 0:
-            self.nodes[-1] = self.nodes[-1] + self.completion
-            self.completion = ''
-            self.refresh()
+        ns = self.model.nodes
+        if len(self.model.completion) > 0:
+            ns[-1] = ns[-1] + self.model.completion
+            self.model.completion = ''
+            self.view.refresh()
         else:
             self.append('')
             if(self.pushed is None):
-                self.pushed = self.nodes[-1]
+                self.pushed = ns[-1]
             m = self.matches[self.candidate]
-            self.nodes[-1] = self.pushed + m[len(self.pushed):len(m)]
-            self.refresh()
+            ns[-1] = self.pushed + m[len(self.pushed):len(m)]
+            self.view.refresh()
             self.candidate = (self.candidate + 1) % len(self.matches)
 
     def advance_editor(self):
-        if self.completion == '':
-            if len(self.nodes)>1:
+        ns = self.model.nodes
+        if self.model.completion == '':
+            if len(ns)>1:
                 self.lookup_path()
             else:
-                self.nodes = self.nodes + ['']
-            self.completion = None
+                ns.append('')
+            self.model.completion = None
 
     def regress_editor(self):
-        if(len(self.nodes) > 1 and self.nodes[-1] == ''):
-            self.nodes = self.nodes[0:(len(self.nodes)-1)]
-            self.nodes[-1] = ''
-        self.completion = None        
+        ns = self.model.nodes
+        if(len(ns) > 1 and ns[-1] == ''):
+            ns.pop()
+            ns[-1] = ''
+        self.model.completion = None        
 
     def append(self, c):
-        n = self.nodes[-1] + c
-        it = self.trie.iteritems(prefix=n)
+        ns = self.model.nodes
+        n = ns[-1] + c
+        it = self.model.trie.iteritems(prefix=n)
         try:
             matches = [elt[0] for elt in it]
             if(self.pushed is None):
@@ -115,26 +132,29 @@ class GPController:
             if len(self.matches) != 0:
                 common = common_prefix(self.matches)
                 if(len(common) >= len(n)):
-                    self.completion = common[len(n):len(common)]
-                    self.nodes[-1] = n
-        except Exception as e:
-            # print(e) #debug
-            self.nodes[-1] = ''
-            self.completion = ''
+                    self.model.completion = common[len(n):len(common)]
+                    ns[-1] = n
+        except KeyError as ke:
+            ns[-1] = ''
+            self.model.completion = ''
 
     def lop_off_char(self):
-        if(self.nodes[-1] != ''):
-            self.nodes[-1] = self.nodes[-1][0:(len(self.nodes[-1])-1)]
+        ns = self.model.nodes
+        if(ns[-1] != ''):
+            ns[-1] = ns[-1][0:(len(ns[-1])-1)]
             self.append('')
 
     def lookup_path(self):
-        v1 = self.graph.vs.select(name=self.nodes[-2])[0]
-        v2 = self.graph.vs.select(name=self.nodes[-1])[0]
-        shortest_paths = self.graph.get_shortest_paths(v1, to=v2, mode='ALL', output='vpath')
+        ns = self.model.nodes
+        g = self.model.graph
+        v1 = g.vs.select(name=ns[-2])[0]
+        v2 = g.vs.select(name=ns[-1])[0]
+        shortest_paths = g.get_shortest_paths(v1, to=v2, mode='ALL', output='vpath')
         if len(shortest_paths) > 0:
             p = shortest_paths[0]
-            ns = self.nodes
-            self.nodes = (
-                ns[0:(len(ns)-2)] +
-                [self.graph.vs[selector]['name'] for selector in p] +
-                [''] )
+            ns.pop()
+            ns.pop()
+            for selector in p:
+                ns.append(g.vs[selector]['name'])    
+            ns.append('')
+
--- source/view.py
+++ source/view.py
@@ -1,9 +1,16 @@
 
+from ansi_codes import *
 from model import GPModel
 
 class GPView():
-    def __init__(self):
-        self.view_state=''
+    def __init__(self, model, vertical_layout, description_capable):
+        self.model = model
+        self.vertical_layout = vertical_layout
+        self.description_capable = description_capable
+        self.reset_state()
+
+    def reset_state(self):
+        self.lines = 1
 
     def display_abridged_cached(self):
         print(self.cached)
@@ -12,16 +19,17 @@ class GPView():
             print(UP_LINE)
 
     def refresh(self):
+        ns = self.model.nodes
         txt = (CLEAR_LINE + '\n')*self.lines + self.lines*UP_LINE
         self.lines = 1
-        for i,n in enumerate(self.nodes):
-            if i == len(self.nodes)-1:
-                if(n != '' and self.completion != None):
+        for i,n in enumerate(ns):
+            if i == len(ns)-1:
+                if(n != '' and self.model.completion != None):
                     txt += BOLD_MAGENTA + n + RESET
-                    txt += MAGENTA + self.completion + RESET
+                    txt += MAGENTA + self.model.completion + RESET
                 txt += CURSOR_CHAR
             else:
-                if i == len(self.nodes)-2:
+                if i == len(ns)-2:
                     divider_str = GREEN + '...' + RESET
                 else:
                     divider_str = ' ' + GREEN + '-' + RESET + ' '
@@ -31,7 +39,7 @@ class GPView():
                     divider =  '\n'
                     self.lines = self.lines + 1
                 txt += BOLD_CYAN + n + RESET
-                if i == len(self.nodes)-2:
+                if i == len(ns)-2:
                     self.cached = txt
                 txt += divider
             if i == 0:
